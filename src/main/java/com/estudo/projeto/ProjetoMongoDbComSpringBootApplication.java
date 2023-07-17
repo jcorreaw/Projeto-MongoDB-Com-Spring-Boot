2f3c2f752b31bbdff0d38c3d5686dcd79d2c910c
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-17 11:22:51 -0300

    Projeto Criado

---
+++ src/main/java/com/estudo/projeto/ProjetoMongoDbComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMongoDbComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMongoDbComSpringBootApplication.class, args);
+	}
+
+}
