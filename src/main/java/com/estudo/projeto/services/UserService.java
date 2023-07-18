ee0238dbb70558d8d6a001e722f885ce1938a049
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-18 15:30:10 -0300

    Inserção de usuário com POST

--- src/main/java/com/estudo/projeto/services/UserService.java
+++ src/main/java/com/estudo/projeto/services/UserService.java
@@ -1,6 +1,7 @@
 package com.estudo.projeto.services;
 
 import com.estudo.projeto.domain.User;
+import com.estudo.projeto.dto.UserDTO;
 import com.estudo.projeto.repository.UserRepository;
 import com.estudo.projeto.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
