04c099b7c1cee3b8d4c5c674527f66719111d951
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-18 11:15:55 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/estudo/projeto/domain/User.java
+++ src/main/java/com/estudo/projeto/domain/User.java
@@ -1,9 +1,16 @@
 package com.estudo.projeto.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+@Document(collection = "user")
+public class User implements Serializable {
+    public static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
