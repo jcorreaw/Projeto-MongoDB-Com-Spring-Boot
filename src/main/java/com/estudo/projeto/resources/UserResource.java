25048d1366589f58053dc9601cdd7579e7dedd38
jcorreaw <juninhocorrea2014@gmail.com>
2023-07-19 11:13:00 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/estudo/projeto/resources/UserResource.java
+++ src/main/java/com/estudo/projeto/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.estudo.projeto.resources;
 
+import com.estudo.projeto.domain.Post;
 import com.estudo.projeto.domain.User;
 import com.estudo.projeto.dto.UserDTO;
 import com.estudo.projeto.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
